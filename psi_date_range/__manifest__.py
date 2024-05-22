ee3587b2f10a44632f3ba755cc958c86e902cb3b
Michał Dzindzio <michaeldzindzio@gmail.com>
2024-05-22 13:33:19 +0200

    add module

---
+++ psi_date_range/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Date Ranges Generator',
+    'summary': 'Module for generating date ranges.',
+    'author': "Psilon Sp. z o.o.",
+    'website': "https://www.psilon.pl",
+    'version': '1.0.0',
+    'license': 'LGPL-3',
+    'category': 'Uncategorized',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'views/ir_sequence_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+        ],
+    },
+    'images': ['static/description/banner.png'],
+    'application': True
+}
